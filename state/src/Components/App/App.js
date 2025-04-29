eaa766a7c268ee1e469f15eef4c16932adfe4418
ChangSeven2k5 <changcoor7@gmail.com>
2025-04-29 08:34:19 +0700

    Done

--- state/src/Components/App/App.js
+++ state/src/Components/App/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Routes is used instead of Switch
-import Add from '../Form_State/Add';
-import ProductList from '../Form_State/ProductList';
+import ProductOrder from '../Menu/ProductOrder';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Routes> {/* Switch is replaced by Routes */}
-            <Route path="/add" element={<Add />} /> {/* Route component now uses element prop */}
-            <Route path="/products" element={<ProductList />} /> {/* Route component now uses element prop */}
-          </Routes>
-        </div>
-      </Router>
+      <div className="App">
+          <ProductOrder></ProductOrder>
+      </div>
+
     );
   }
 }
