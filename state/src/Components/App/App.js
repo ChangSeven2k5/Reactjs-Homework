4c54a2d4011239e08b1a0ea1aabede90f778405f
ChangSeven2k5 <changcoor7@gmail.com>
2025-04-29 08:54:02 +0700

    Done

--- state/src/Components/App/App.js
+++ state/src/Components/App/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Add from "../Form_State/Add";  
+import Products from "../Form_State/Products";  
 import ProductOrder from '../Menu/ProductOrder';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-          <ProductOrder></ProductOrder>
-      </div>
-
+      <Router>
+        <Routes>
+          {/* Dùng Routes thay cho Switch, element thay cho component ( ở version 6) */}
+          <Route path="/" element={<Products />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </Router>
+      // <ProductOrder></ProductOrder>
     );
   }
 }
 
 export default App;
+
